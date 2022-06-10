39a5922ef135de9b3147b4fa7e8e6a039423d16e
user2410 <dc241024@gmail.com>
2022-06-10 11:22:43 +0700

    css_module_styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 
 import AllMeetupsPage from './pages/AllMeetups';
-// import NewMeetupsPage from './pages/NewMeetup';
-// import FavoritesPage from './pages/Favorites';
+import NewMeetupsPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   // domain: loaclhost:3000/
 
   return (
   <div>
-    <BrowserRouter>
+      <MainNavigation />
       <Routes>
-        <Route path="/" element={<AllMeetupsPage />} />
+        {/* <Route path="/" element={<LayoutPage />}> */}
+        <Route index element={<AllMeetupsPage />} />
+        <Route path="meetups" element={<NewMeetupsPage />} />
+        <Route path="favs" element={<FavoritesPage />} />
+        {/* </Route> */}
       </Routes>
-    </BrowserRouter>
   </div>
   );
 }
