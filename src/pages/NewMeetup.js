6fa606274546d5714f48f9e1045230d635adeef0
user2410 <dc241024@gmail.com>
2022-06-13 14:33:16 +0700

    Add a submit form

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,12 @@
+import NewMeetupForm from "../components/meetups/NewMeetupFrom";
+
 function NewMeetupsPage(){
-    return <div>NewMeetups Page</div>
+    return (
+        <section>
+            <h1>Add new meetup</h1>
+            <NewMeetupForm />
+        </section>
+    )
 }
 
 export default NewMeetupsPage;
