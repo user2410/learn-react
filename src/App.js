97531943fc16b688a9407ea0e8351034d662f631
user2410 <dc241024@gmail.com>
2022-06-14 14:43:21 +0700

    fresh project

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import Todo from "./components/Todo";
 function App() {
   return (
   <div>
-    <h1>Todos</h1>
-    <Todo text='Learn React'/>
-    <Todo text='Learn React1'/>
-    <Todo text='Learn React2'/>
   </div>);
 }
 
